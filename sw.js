9936a188f456bb39dbd979323c6bf64aa655d8b6
imad limrani <148809071+gpldroid@users.noreply.github.com>
2026-09-22 16:53:38 +0100

    Bust stale mPanel service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mpanel-shell-v1";
+const CACHE="mpanel-shell-v2";
 const APP_SHELL=["./","./index.html","./assets/css/style.css"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
